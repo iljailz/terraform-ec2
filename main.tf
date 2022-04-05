49f764bdececf59cb243543fc6012c31d4e0fcf1
Ilja Verbovetski <96315345+iljailz@users.noreply.github.com>
2022-04-05 12:16:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_key_pair" "my-key" {
-  key_name   = var.key_name
-  public_key = var.public_key
-}
+#resource "aws_key_pair" "my-key" {
+#  key_name   = var.key_name
+#  public_key = var.public_key
+#}
 
 resource "aws_instance" "web" {
-  key_name = aws_key_pair.my-key.key_name
+ # key_name = aws_key_pair.my-key.key_name
   instance_type = var.instance_type
   ami           = var.ami_id
   security_groups = [aws_security_group.web.id]
