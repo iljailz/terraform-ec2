7c17d04248e75662a4243d0f5ee33a3ccd0c7fcd
Ilja Verbovetski <96315345+iljailz@users.noreply.github.com>
2022-04-05 13:21:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "web" {
  # key_name = aws_key_pair.my-key.key_name
   instance_type = var.instance_type
   ami           = var.ami_id
-  #security_groups = [aws_security_group.web.id]
+  security_groups = aws_security_group.jenkins
 
   #subnet_id     = tolist(data.aws_subnet_ids.my-subnets.ids)[0]
 
